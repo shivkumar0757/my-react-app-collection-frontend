8fec18bb23ca684aa398129bfe44bd56da9f9144
shivkumar0757 <shivkumar0757@gmail.com>
2023-01-28 16:39:23 +0530

    page

--- src/apps/AlHome/AlHome.js
+++ src/apps/AlHome/AlHome.js
@@ -4,12 +4,15 @@ import './AlHome.css';
 function AlHome () {
     return (
         <div className="container">
+            <div className="header">
+                <h2>Assets Vs Liabilities</h2>
+            </div>
       <div className="upper">
         <div className="upper-upper">
-          <h1>Income</h1>
+          <h1  contenteditable="true">Income</h1>
         </div>
         <div className="upper-lower">
-          <h1>Expenseds</h1>
+          <h1 contenteditable="true">Expenses</h1>
         </div>
       </div>
       <div className="lower">
